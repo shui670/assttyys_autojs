9bf49ee1ddb706915c99eca3145197a1e0e0fa26
shui670 <810518262@qq.com>
2026-06-01 21:07:48 +0800

    修复悬浮窗不显示

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,32 @@ import webviewEvents from '@/system/webviewEvents';
 import InputHideUtil from '@/system/inputhideutil';
 import { storeCommon } from '@/system/Store/store';
 import drawFloaty from '@/system/drawFloaty';
-import { doInitHookConsoleLog, getWebLoaded } from './common/toolAuto';
+import { doInitHookConsoleLog, requestMyScreenCapture, setWebLoaded } from './common/toolAuto';
+import helperBridge from '@/system/helperBridge';
+import MyAutomator from '@/system/MyAutomator';
 
 webviewEvents();
+
+const storeSettings = storeCommon.get('settings', {});
+if (!storeSettings.tapType) {
+	if (device.sdkInt >= 24) {
+		storeSettings.tapType = '无障碍';
+	} else {
+		storeSettings.tapType = 'Root';
+	}
+	storeCommon.put('settings', storeSettings);
+}
+helperBridge.setAutomator(new MyAutomator(storeSettings.tapType));
+requestMyScreenCapture((success: boolean) => {
+	if (success) {
+		setWebLoaded(true);
+	}
+}, helperBridge);
 
 // effect$是作业线程，当core的权限全部到位后，effect$才开始运作
 effect$.subscribe(() => {
 	// 监听放在effect里，只有当权限到位后，监听才生效
-	if (floaty.checkPermission() && getWebLoaded()) {
+	if (floaty.checkPermission()) {
 		myFloaty.init();
 	}
 	const storeSettings = storeCommon.get('settings', {});
